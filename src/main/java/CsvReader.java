007ad762ec400aca63b569cac0a031bcc719f0f0
Pawel Wiatr <p.wiatr93@gmail.com>
2023-02-12 15:10:07 +0000

    Csv reader class added

---
+++ src/main/java/CsvReader.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+
+public class CsvReader {
+
+    private List<Meal> meals;
+
+    public CsvReader() {
+        this.meals = new ArrayList<Meal>();
+    }
+
+    public List<Meal> readAndSaveToArrayList(String filePath) throws FileNotFoundException {
+        meals = new CsvToBeanBuilder(new FileReader(filePath))
+                .withType(Meal.class)
+                .build()
+                .parse();
+
+        return meals;
+    }
+}
